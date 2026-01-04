af48cd5dad161c8ded53a8ff65f444450ab86355
fiftypounmule <shaun5han@outlook.com>
2026-01-04 10:36:21 +0800

    Initial Commit

---
+++ KinoTaste/KinoTasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinoTasteApp.swift
+//  KinoTaste
+//
+//  Created by Boxiang Shan on 2026/1/4.
+//
+
+import SwiftUI
+
+@main
+struct KinoTasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KinoTasteTests/KinoTasteTests.swift
@@ -0,0 +1,17 @@
+//
+//  KinoTasteTests.swift
+//  KinoTasteTests
+//
+//  Created by Boxiang Shan on 2026/1/4.
+//
+
+import Testing
+@testable import KinoTaste
+
+struct KinoTasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
